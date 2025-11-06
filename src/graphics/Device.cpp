05ae098f139b7396b8ffc02f497f516187202b2e
KirillMarfoz <147299219+KirillMarfoz@users.noreply.github.com>
2025-11-06 21:28:33 +1200

    Device has been created!

--- src/graphics/Device.cpp
+++ src/graphics/Device.cpp
@@ -2,18 +2,77 @@
 
 #include "main/SDebug.hpp"
 
-Device::Device(VkInstance vk_instance, DGraphicsSettings settings) {
+Device::Device(VkInstance vk_instance, DGraphicsSettings settings) : debug(DDebugSettings{}) {
+    //PICKING A PHYSICAL DEVICE
     uint32_t device_count = 0;
     vkEnumeratePhysicalDevices(vk_instance, &device_count, nullptr);
 
-    //need to move to debug
     if (device_count == 0)
         SDebug::self->ferr("VULKAN::DEVICE", "No suitable devices!", "VULKAN::DEVICES::INIT");
 
     std::vector<VkPhysicalDevice> devices(device_count);
     vkEnumeratePhysicalDevices(vk_instance, &device_count, devices.data());
+
+    for (const auto& device : devices) {
+        if (isDeviceSuitable(device)) {
+            physical_device = device;
+            break;
+        }
+    }
+
+    if (physical_device == VK_NULL_HANDLE) {
+        throw std::runtime_error("failed to find a suitable GPU!");
+    }
+
+    //PICKING A LOGICAL DEVICE
+    QueueFamilyIndices family_indices = pickFamilyQueues(physical_device);
+    VkPhysicalDeviceFeatures features{};
+    VkDeviceQueueCreateInfo queueCreateInfo{};
+    VkDeviceCreateInfo createDeviceInfo{};
+
+    queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+    queueCreateInfo.queueFamilyIndex = family_indices.graphicsFamily.value();
+    queueCreateInfo.queueCount = 1;
+    float queuePriority = 1.0f;
+    queueCreateInfo.pQueuePriorities = &queuePriority;
+
+    createDeviceInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+    createDeviceInfo.pQueueCreateInfos = &queueCreateInfo;
+    createDeviceInfo.queueCreateInfoCount = 1;
+    createDeviceInfo.pEnabledFeatures = &features;
+    createDeviceInfo.enabledExtensionCount = 0;
+    createDeviceInfo.enabledLayerCount = static_cast<uint32_t>(layers.size());
+    createDeviceInfo.ppEnabledLayerNames = layers.data();
+
+    if (vkCreateDevice(physical_device, &createDeviceInfo, nullptr, &picked_device) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create logical device!");
+    }
+    vkGetDeviceQueue(picked_device, family_indices.graphicsFamily.value(), 0, &graphicsQueue);
 }
 
 Device::~Device() {
+    vkDestroyDevice(picked_device, nullptr);
 
+}
+
+Device::QueueFamilyIndices Device::pickFamilyQueues(VkPhysicalDevice physical_device) {
+    QueueFamilyIndices family_indices;
+
+    uint32_t queue_family_count = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(physical_device, &queue_family_count, nullptr);
+
+    std::vector<VkQueueFamilyProperties> queueFamilies(queue_family_count);
+    vkGetPhysicalDeviceQueueFamilyProperties(physical_device, &queue_family_count, queueFamilies.data());
+
+    int i;
+    for (const auto& queueFamily : queueFamilies) {
+        if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+            family_indices.graphicsFamily = i;
+        }
+
+        if (family_indices.is_completed()) {
+            break;
+        }
+        i++;
+    }
 }
--- src/graphics/Device.hpp
+++ src/graphics/Device.hpp
@@ -2,6 +2,7 @@
 #define DOOMCPP_DEVICES_H
 
 #include <vulkan/vulkan.hpp>
+#include "../main/SDebug.hpp"
 #include "main/Data.hpp"
 
 class Device {
@@ -10,14 +11,35 @@ public:
     ~Device();
 
     VkDevice picked_device;
+    VkPhysicalDevice physical_device;
     VkPhysicalDeviceProperties device_properties;
 
 private:
+    struct QueueFamilyIndices {
+        std::optional<uint32_t> graphicsFamily;
+
+        inline bool is_completed() {
+            return graphicsFamily.has_value();
+        }
+    };
+
+    inline bool isDeviceSuitable(VkPhysicalDevice device) {
+        QueueFamilyIndices indices = pickFamilyQueues(device);
+
+        return indices.is_completed();
+    }
+
     std::vector<VkPhysicalDevice> getPhysicalDevices();
-    VkPhysicalDevice pickPhysicalDevice(std::vector<VkPhysicalDevice> from);
+    //VkPhysicalDevice pickPhysicalDevice(std::vector<VkPhysicalDevice> from);
     std::vector<VkQueueFamilyProperties> getQueuesProperties();
-    std::vector<VkDeviceQueueCreateInfo> pickQueues();
+    QueueFamilyIndices pickFamilyQueues(VkPhysicalDevice physical_device);
     void createDevice();
+
+    std::optional<uint32_t> graphicsFamily;
+    VkQueue graphicsQueue;
+
+    SDebug debug;
+    std::vector<const char*> layers = debug.getValidationLayers();
 };
 
 
